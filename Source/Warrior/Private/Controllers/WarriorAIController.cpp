e7d7b0a0f6828d50237c76b95d5ba87c98b27a95
kht9544 <kht9544@naver.com>
2025-12-13 15:05:41 +0900

    Add WarriorAIController and AI perception setup
    Introduced AWarriorAIController with crowd following and AI sight perception components. Added new AI controller assets for enemies and updated Gruntling blueprints. Modified project dependencies to include AIModule.

---
+++ Source/Warrior/Private/Controllers/WarriorAIController.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Controllers/WarriorAIController.h"
+#include "Navigation/CrowdFollowingComponent.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+
+#include "WarriorDebugHelper.h"
+
+AWarriorAIController::AWarriorAIController(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer.SetDefaultSubobjectClass<UCrowdFollowingComponent>("PathFollowingComponent"))
+{
+	if (UCrowdFollowingComponent* CrowdComp = Cast<UCrowdFollowingComponent>(GetPathFollowingComponent()))
+	{
+		Debug::Print(TEXT("CrowdFollowingCOmponent valid"),FColor::Green);
+	}
+
+    AISenseConfig_Sight = CreateDefaultSubobject<UAISenseConfig_Sight>("EnemySenseConfig_Sight");
+    AISenseConfig_Sight->DetectionByAffiliation.bDetectEnemies = true;
+    AISenseConfig_Sight->DetectionByAffiliation.bDetectFriendlies = false;
+    AISenseConfig_Sight->DetectionByAffiliation.bDetectNeutrals = false;
+    AISenseConfig_Sight->SightRadius = 5000.f;
+    AISenseConfig_Sight->LoseSightRadius = 0.f;
+    AISenseConfig_Sight->PeripheralVisionAngleDegrees = 360.f;
+
+    EnemyPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>("EnemyPerceptionComponent");
+    EnemyPerceptionComponent->ConfigureSense(*AISenseConfig_Sight);
+    EnemyPerceptionComponent->SetDominantSense(UAISenseConfig_Sight::StaticClass());
+    EnemyPerceptionComponent->OnTargetPerceptionUpdated.AddUniqueDynamic(this, &AWarriorAIController::OnEnemyPerceptionUpdated);
+
+}
+
+void AWarriorAIController::OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
+{
+	
+}
---
+++ Source/Warrior/Public/Controllers/WarriorAIController.h
@@ -0,0 +1,35 @@
+
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "WarriorAIController.generated.h"
+
+class UAIPerceptionComponent;
+class UAISenseConfig_Sight;
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API AWarriorAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	AWarriorAIController(const FObjectInitializer& ObjectInitializer);
+
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UAIPerceptionComponent* EnemyPerceptionComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UAISenseConfig_Sight* AISenseConfig_Sight;
+
+	UFUNCTION()
+	virtual void OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
+};
+
+
